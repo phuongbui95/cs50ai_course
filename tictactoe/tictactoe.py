9fad352e97514f28571aff9279e518965355d138
PB Github <phuong.buitan95@gmail.com>
2025-05-31 22:43:47 +0700

    player, actions

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -12,9 +12,6 @@ EMPTY = None
 def initial_state():
     """
     Returns starting state of the board.
-    # X gets the first move.
-    # Case 01: Randomly picks a position => do this first!
-    # Case 02: Choose a position by input => just an additional option
     """
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
@@ -25,28 +22,60 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    # Count the number of X's and O's on the board
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+    
+    # Remember X is always the first player
+    if terminal(board) == False: # game is NOT over
+        return X if x_count == 0 or x_count == o_count else return O
+    
+    return
+    # raise NotImplementedError
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    available_actions = set()
+    if terminal(board) == False: # game is in progress
+        for i,j in enumerate(board):
+            available_actions.add((i,j)) if board[i][j] == EMPTY
+
+    return available_actions            
+    # raise NotImplementedError
 
 
+# actions function is needed
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     # remember to make a deep copy of board before going to the next change.
     """
+
+    # deep copy the board before adding new action
+
+    # udpate new action = the last element of the actions set
+
     raise NotImplementedError
 
-
+# result function is needed
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+
+    # Count the number of X's and O's on the board
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+    empty_count = sum(row.count(EMPTY) for row in board)
+    
+    # if winner, else none
+    # X is the winner (x_count>=3)
+    # O is the winner (o_count>=3)
+
+
     raise NotImplementedError
 
 

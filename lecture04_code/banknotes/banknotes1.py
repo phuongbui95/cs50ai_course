be54562efa055539cd594ab9f9061769ee1063b6
PB Github <phuong.buitan95@gmail.com>
2025-07-17 21:56:11 +0700

    add new files

--- lecture04_code/banknotes/banknotes1.py
+++ lecture04_code/banknotes/banknotes1.py
@@ -1,14 +1,14 @@
 import csv
 import random
 
-from sklearn import svm
+from sklearn.svm import SVC
 from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 
 model = Perceptron()
-# model = svm.SVC()
+# model = SVC()
 # model = KNeighborsClassifier(n_neighbors=1)
 # model = GaussianNB()
 
